cb54ce537008cd7f6a49b46704d26ee1773cfdad
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-08-14 08:50:26 +0900

    CompletableFuture - 콜백 구현해보기

---
+++ functional-java/src/test/java/com/yoon/TExpress/completableFuture/CompletableFutureTest.java
@@ -0,0 +1,48 @@
+package com.yoon.TExpress.completableFuture;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureTest {
+
+    @Test
+    void thenApply_결과_값을_받아서_다른_타입으로_변경() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello " + Thread.currentThread().getName());
+            return "Hello";
+        }).thenApply((s) -> {
+            System.out.println(Thread.currentThread().getName());
+            return s.toLowerCase();
+        });
+
+        future.get();
+
+    }
+
+    @Test
+    void then_Accept_결과_값을_받아서_또_다른_실행을_하는_그러나_return_값이_없는() throws ExecutionException, InterruptedException {
+        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello " + Thread.currentThread().getName());
+            return "Hello";
+        }).thenAccept((s) -> {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(s.toUpperCase());
+        });
+
+        future.get();
+    }
+
+    @Test
+    void then_Run_결과_값을_참조하지_않고_단순_실행() throws ExecutionException, InterruptedException {
+        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello " + Thread.currentThread().getName());
+            return "Hello";
+        }).thenRun(() -> {
+            System.out.println(Thread.currentThread().getName());
+        });
+
+        future.get();
+    }
+}
