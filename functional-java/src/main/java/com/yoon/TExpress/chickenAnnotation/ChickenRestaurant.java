8fb1f2b8d3714843d092ee660f2ef5d60afadf81
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-08-16 11:10:06 +0900

    Chicken Annotation with TYPE_USE

--- functional-java/src/main/java/com/yoon/TExpress/chickenAnnotation/ChickenRestaurant.java
+++ functional-java/src/main/java/com/yoon/TExpress/chickenAnnotation/ChickenRestaurant.java
@@ -1,7 +1,14 @@
 package com.yoon.TExpress.chickenAnnotation;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Chicken
 public class ChickenRestaurant<@Chicken T> {
 
+    public static void main(@Chicken String[] args) throws @Chicken RuntimeException{
+        List<@Chicken String> names = Arrays.asList("yoonji");
+    }
     public static <@Chicken C> void print(C c){
         System.out.println(c);
     }
