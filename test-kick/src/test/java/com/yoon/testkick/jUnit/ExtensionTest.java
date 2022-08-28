ab6f0a8a84b31c486508a2de4327fee2838a1bce
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-08-29 08:19:36 +0900

    ExtensionTest

---
+++ test-kick/src/test/java/com/yoon/testkick/jUnit/ExtensionTest.java
@@ -0,0 +1,23 @@
+package com.yoon.testkick.jUnit;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(FindSlowTestExtension.class)
+public class ExtensionTest {
+
+    @Test
+    @FastTest
+    void basic_test_1() {
+        System.out.println("Basic basic_test_1");
+    }
+
+    @Test
+    @FastTest
+    void basic_test_2() throws InterruptedException {
+        Thread.sleep(1005L);
+        System.out.println("Basic basic_test_2");
+    }
+
+}
