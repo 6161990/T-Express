ff51497c332405867d3eed6a69a22cbbbce4379c
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-08-28 10:13:20 +0900

    Create FindSlowTestExtension.java
    jUnit4 extension implements

---
+++ test-kick/src/test/java/com/yoon/testkick/jUnit/FindSlowTestExtension.java
@@ -0,0 +1,35 @@
+package com.yoon.testkick.jUnit;
+
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
+
+public class FindSlowTestExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+    private static final long THRESHOLD = 1000L;
+
+    @Override
+    public void beforeTestExecution(ExtensionContext context) throws Exception {
+        Store store = getStore(context);
+        store.put("START_TIME", System.currentTimeMillis());
+    }
+
+    @Override
+    public void afterTestExecution(ExtensionContext context) throws Exception {
+        String testMethodName = context.getRequiredTestMethod().getName();
+        Store store = getStore(context);
+        Long start_time = store.remove("START_TIME", long.class);
+        long duration = System.currentTimeMillis() - start_time;
+        if(duration > THRESHOLD){
+            System.out.printf("please consider mark method [%s] with @SlowTest.\n", testMethodName);
+        }
+    }
+
+    private Store getStore(ExtensionContext context) {
+        String testClassName = context.getRequiredTestClass().getName();
+        String testMethodName = context.getRequiredTestMethod().getName();
+        return context.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+    }
+
+}
