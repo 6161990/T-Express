9be67ef8bf9f890af20a7257294446127a082cca
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-09-07 07:43:22 +0900

    stubbing practice1

---
+++ test-kick/src/test/java/com/yoon/testkick/mockito/study/StudyServiceTest.java
@@ -0,0 +1,40 @@
+package com.yoon.testkick.mockito.study;
+
+
+import com.yoon.testkick.mockito.domain.Member;
+import com.yoon.testkick.mockito.domain.Study;
+import com.yoon.testkick.mockito.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StudyServiceTest {
+
+    @Mock
+    MemberService memberService;
+
+    @Mock
+    StudyRepository repository;
+
+    @Test
+    void stubbing_practice() {
+        StudyService studyService = new StudyService(memberService, repository);
+        Member member = new Member();
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        Study study = new Study(10, "테스트");
+        when(repository.save(study)).thenReturn(study);
+
+        Study resultStudy = studyService.createNewStudy(1L, study);
+
+        assertNotNull(resultStudy.getOwner());
+        assertThat(resultStudy.getOwner()).isEqualTo(member);
+    }
+}
