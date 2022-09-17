f464efb1cf6643c05a9608fbc12b2f832f049f5b
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-09-17 10:25:53 +0900

    create member Service

--- test-kick/src/main/java/com/yoon/testkick/mockito/member/MemberService.java
+++ test-kick/src/main/java/com/yoon/testkick/mockito/member/MemberService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface MemberService {
 
     Optional<Member> findById(Long memberId) throws MemberNotFoundException;
+    void save(Member member);
     void validate(Long MemberId);
     void notify(Study study);
     void notify(Member member);
