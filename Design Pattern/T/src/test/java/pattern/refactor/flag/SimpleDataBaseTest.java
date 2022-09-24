a24d45b515a071784359b35a91290012e3757201
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-09-24 12:50:08 +0900

    Refactor SimpleDataBase

---
+++ Design Pattern/T/src/test/java/pattern/refactor/flag/SimpleDataBaseTest.java
@@ -0,0 +1,24 @@
+package pattern.refactor.flag;
+
+import org.junit.jupiter.api.Test;
+import pattern.refactor.flag.before.SimpleDataBase;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+class SimpleDataBaseTest {
+
+    @Test
+    void before() throws IOException {
+        SimpleDataBase simpleDataBase = new SimpleDataBase(new FileReader("/Users/6161990src/Documents/GitHub/T-Express/Design Pattern/T/src/test/java/pattern/refactor/flag/test.txt"));
+
+        Iterator<String> ir = simpleDataBase.iterator();
+
+        while (ir.hasNext()){
+            String key = ir.next();
+            System.out.println("key:" + key);
+            System.out.println("value:" + simpleDataBase.getValue(key));
+        }
+    }
+}
