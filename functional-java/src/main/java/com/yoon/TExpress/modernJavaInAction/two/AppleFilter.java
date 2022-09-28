dff6183e106343dc14d7ddea0718d4ba1d930bc0
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-09-29 08:35:56 +0900

    Two. 동작 파라미터화 코드 전달하기
    첫번째시도 : 녹색 사과필터링
     -> 근데 빨간색도 필터링 하고 싶다면?

---
+++ functional-java/src/main/java/com/yoon/TExpress/modernJavaInAction/two/AppleFilter.java
@@ -0,0 +1,19 @@
+package com.yoon.TExpress.modernJavaInAction.two;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.yoon.TExpress.modernJavaInAction.two.Color.GREEN;
+
+public class AppleFilter {
+
+    public static List<Apple> filterGreenApples(List<Apple> inventory){
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory){
+            if(GREEN.equals(apple.getColor())){
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+}
---
+++ functional-java/src/main/java/com/yoon/TExpress/modernJavaInAction/two/Color.java
@@ -0,0 +1,6 @@
+package com.yoon.TExpress.modernJavaInAction.two;
+
+public enum Color {
+    RED,
+    GREEN
+}
