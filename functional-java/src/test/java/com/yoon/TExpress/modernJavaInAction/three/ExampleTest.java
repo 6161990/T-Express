7a11686b05fc0652a2ab6d0a6f2182d3246ceb86
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-10-04 08:17:17 +0900

    SupplierTest

--- functional-java/src/test/java/com/yoon/TExpress/modernJavaInAction/three/ExampleTest.java
+++ functional-java/src/test/java/com/yoon/TExpress/modernJavaInAction/three/ExampleTest.java
@@ -1,11 +1,16 @@
 package com.yoon.TExpress.modernJavaInAction.three;
 
+import com.yoon.TExpress.modernJavaInAction.two.Apple;
+import com.yoon.TExpress.modernJavaInAction.two.Color;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
+import static com.yoon.TExpress.modernJavaInAction.two.Color.RED;
 import static java.util.Arrays.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ExampleTest {
@@ -21,4 +26,14 @@ public class ExampleTest {
         return predicate.test(stringList);
     }
 
+    @Test
+    void SupplierTest() {
+        Apple apple = create(() -> new Apple(10, RED));
+
+        assertThat(apple.getColor()).isEqualTo(RED);
+    }
+
+    private Apple create(Supplier<Apple> supplier) {
+        return supplier.get();
+    }
 }
