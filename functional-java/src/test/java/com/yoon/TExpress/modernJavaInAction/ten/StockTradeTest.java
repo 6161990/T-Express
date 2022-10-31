e841d417b3e1e5bd79f62c4674ecd09c0c2c55e1
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-10-31 10:16:37 +0900

    장황한 도메인 객체 생성하기 step 1

---
+++ functional-java/src/test/java/com/yoon/TExpress/modernJavaInAction/ten/StockTradeTest.java
@@ -0,0 +1,36 @@
+package com.yoon.TExpress.modernJavaInAction.ten;
+
+import org.junit.jupiter.api.Test;
+
+class StockTradeTest {
+
+    @Test
+    void step1_장황한_주문만들기() {
+        Order order = new Order();
+        order.setCustomer("BigBank");
+
+        Trade trade1 = new Trade();
+        trade1.setTradeType(TradeType.BUY);
+
+        Stock stock1 = new Stock();
+        stock1.setSymbol("IBM");
+        stock1.setMarket("NYSE");
+
+        trade1.setStock(stock1);
+        trade1.setPrice(125.00);
+        trade1.setQuantity(80);
+        order.addTrader(trade1);
+
+        Trade trade2 = new Trade();
+        trade2.setTradeType(TradeType.BUY);
+
+        Stock stock2 = new Stock();
+        stock2.setSymbol("GOOGLE");
+        stock2.setMarket("NASDAQ");
+
+        trade2.setStock(stock2);
+        trade2.setPrice(375.00);
+        trade2.setQuantity(50);
+        order.addTrader(trade2);
+    }
+}
