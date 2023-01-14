b6dc314009285448bed17b6958ea75bb37488a4c
YoonjiKim <6161990src@gmail.com>
2023-01-14 21:58:32 +0900

    boardingExpress init

---
+++ boarding-express/board/src/main/java/com/yoon/boardingExpress/BoardingExpressApplication.java
@@ -0,0 +1,12 @@
+package com.yoon.boardingExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardingExpressApplication.class, args);
+    }
+}
---
+++ boarding-express/board/src/test/java/com/yoon/boardingExpress/BoardingExpressApplicationTests.java
@@ -0,0 +1,12 @@
+package com.yoon.boardingExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
