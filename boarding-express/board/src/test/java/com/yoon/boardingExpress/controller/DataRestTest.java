bf4fbe76ed8c5cfd268537b15847a761e3e882f3
YoonjiKim <6161990src@gmail.com>
2023-01-19 16:46:43 +0900

    #2 - Data Rest 를 이용한 통합 테스트

---
+++ boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/DataRestTest.java
@@ -0,0 +1,57 @@
+package com.yoon.boardingExpress.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data Rest 를 이용한 통합 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void 게시글_리스트_조회() throws Exception {
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+    }
+
+    @Test
+    void 게시글_단건_조회() throws Exception {
+        mockMvc.perform(get("/api/articles/100"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+    }
+
+    @Test
+    void 게시글_댓글_리스트_조회() throws Exception {
+        mockMvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+    }
+
+    @Test
+    void 게시글_댓글_단건_조회() throws Exception {
+        mockMvc.perform(get("/api/articleComments/100"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+    }
+}
