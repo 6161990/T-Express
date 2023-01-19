4997ae541eeab305e2193608ea39fa9a17ff0c7e
YoonjiKim <6161990src@gmail.com>
2023-01-19 15:50:29 +0900

    #2 - @RepositoryRestResource 추가

--- boarding-express/board/src/main/java/com/yoon/boardingExpress/repository/ArticleRepository.java
+++ boarding-express/board/src/main/java/com/yoon/boardingExpress/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.yoon.boardingExpress.repository;
 
 import com.yoon.boardingExpress.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
--- boarding-express/board/src/main/java/com/yoon/boardingExpress/repository/WriterRepository.java
+++ boarding-express/board/src/main/java/com/yoon/boardingExpress/repository/WriterRepository.java
@@ -2,6 +2,8 @@ package com.yoon.boardingExpress.repository;
 
 import com.yoon.boardingExpress.domain.Writer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface WriterRepository extends JpaRepository<Writer, Long> {
 }
