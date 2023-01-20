e4c0fdff47698497e83c9e3e3858cc95e71bcbf4
6161990 <6161990src@gmail.com>
2023-01-20 16:22:33 +0900

    #6 - 뷰 컨트롤러 테스트 검증 추가

--- boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/ArticleControllerTest.java
+++ boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/ArticleControllerTest.java
@@ -23,6 +23,7 @@ class ArticleControllerTest {
         mockMvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
 
@@ -31,20 +32,24 @@ class ArticleControllerTest {
         mockMvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
     }
 
     @Test
     void article_search_page() throws Exception {
         mockMvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
     }
 
     @Test
     void article_search_hashtag_page() throws Exception {
         mockMvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));
     }
 }
