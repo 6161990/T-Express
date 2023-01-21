b3ff3b31a01b879c51a9ea7f6012d9cbae1fe805
6161990 <6161990src@gmail.com>
2023-01-21 16:32:49 +0900

    #6 - 시큐리티 설정 적용 to test

--- boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/AuthControllerTest.java
+++ boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/AuthControllerTest.java
@@ -1,8 +1,10 @@
 package com.yoon.boardingExpress.controller;
 
+import com.yoon.boardingExpress.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -11,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@Import(SecurityConfig.class)
 public class AuthControllerTest {
 
     @Autowired
