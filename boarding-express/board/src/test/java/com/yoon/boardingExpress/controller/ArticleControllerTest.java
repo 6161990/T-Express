d3ee40fd8e260fd0f925a47a6829cf7a5b9065e9
6161990 <6161990src@gmail.com>
2023-01-21 16:30:52 +0900

    #6 - 로그인 페이지 구현, 스프링 시큐리티 적용

--- boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/ArticleControllerTest.java
+++ boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/ArticleControllerTest.java
@@ -1,16 +1,19 @@
 package com.yoon.boardingExpress.controller;
 
+import com.yoon.boardingExpress.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Import(SecurityConfig.class )
 @DisplayName("view pages")
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
---
+++ boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/AuthControllerTest.java
@@ -0,0 +1,25 @@
+package com.yoon.boardingExpress.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void login_page() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
