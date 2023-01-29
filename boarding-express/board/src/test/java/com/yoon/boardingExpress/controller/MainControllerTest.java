351c20cc787113b04d477f19ac7fe8f5772867f4
6161990 <6161990src@gmail.com>
2023-01-29 14:52:20 +0900

    redirect -> forward

--- boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/MainControllerTest.java
+++ boarding-express/board/src/test/java/com/yoon/boardingExpress/controller/MainControllerTest.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Import(SecurityConfig.class)
 @WebMvcTest(MainController.class)
@@ -21,7 +22,9 @@ class MainControllerTest {
     @Test
     void 루트페이지로_접근하면_articles_페이지로_리다이렉트합니다() throws Exception {
         mockMvc.perform(get("/"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/articles"));
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
