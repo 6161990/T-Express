87d30a652eb0c3a56c5e3b314935304630286fc9
6161990 <6161990src@gmail.com>
2023-04-16 13:29:48 +0900

    #12 - spring securiry config

--- boarding-express/board/src/main/java/com/yoon/boardingExpress/config/SecurityConfig.java
+++ boarding-express/board/src/main/java/com/yoon/boardingExpress/config/SecurityConfig.java
@@ -1,8 +1,17 @@
 package com.yoon.boardingExpress.config;
 
+import com.yoon.boardingExpress.dto.UserAccountDto;
+import com.yoon.boardingExpress.dto.security.BoardPrincipal;
+import com.yoon.boardingExpress.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +20,35 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return  httpSecurity
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin()
-                .and().build();
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                         .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin().and()
+                .logout()
+                    .logoutSuccessUrl("/").and()
+                .build();
     }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Exist The User. User Name is " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
